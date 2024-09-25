3267e210479c967524989755969eddbe957bec25
josuereydev <161535048+josuereydev@users.noreply.github.com>
2024-09-25 11:22:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,6 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"] # Canonical
 }
 
-provider "aws" {
-  region  = "us-east-2"
-}
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
