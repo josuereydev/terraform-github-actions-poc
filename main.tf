1889df199300a23dd04c48a00c604645ee975081
josuereydev <161535048+josuereydev@users.noreply.github.com>
2024-09-24 23:16:22 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+    
+    owners = ["099720109477"] # Canonical
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.ec2_name
+  }
+}
